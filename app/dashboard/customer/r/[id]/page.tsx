cc8805581e1a13a86ce586991755a29fc3cf1a3e
Vince Gabriel Flores <vincegabriel.flores@gmail.com>
2026-01-21 12:52:43 -0500

    feat: implement data fetching for menu items and restaurant details in BlogPostPage

--- app/dashboard/customer/r/[id]/page.tsx
+++ app/dashboard/customer/r/[id]/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { ShoppingCart } from 'lucide-react'
-import { use, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import { Button } from '../../../../../components/ui/button'
 import { Card, CardAction, CardDescription, CardHeader, CardTitle } from '../../../../../components/ui/card'
+import { createClient } from '../../../../../lib/supabase/client'
 
 type MenuItem = {
     created_at: string
@@ -46,6 +47,36 @@ export default function BlogPostPage({
     const { id } = use(params)
     const [menuItems, setMenuItems] = useState(menuItemsStub)
     const [resDetails, setResDetails] = useState(resDetailsStub)
+
+    useEffect(() => {
+        
+        const getMnuList = async () => {
+            const supabase = createClient()
+            const res = await supabase
+                .from("menu-item")
+                .select("*").eq("restaurant-id", Number(id))
+            if (!res.error) {
+                setMenuItems(res.data)
+                console.log({
+                    menuList: res.data
+                })
+            }else{
+                console.log(res.error)
+            }
+        }
+        const getResDetails = async () => {
+            const supabase = createClient()
+            const res = await supabase.from("restaurant").select("*").eq("id", Number(id))
+            if (!res.error) {
+                setResDetails(res.data[0])
+            }else {
+                console.log(res.error)
+            }
+        }
+        getMnuList()
+        getResDetails()
+    }, [])
+
     const handleAddtoCart = async () => {
 
     }
@@ -57,11 +88,12 @@ export default function BlogPostPage({
             </div>
             <div>
                 {
-                    menuItemsStub.map(i => (
+                    menuItems.map(i => (
                         <Card key={i.id}>
                             <CardHeader>
-                                <CardTitle>{i.name}</CardTitle>
+                                <CardTitle>{i.name} $ {i.price}</CardTitle>
                                 <CardDescription>{i.description} </CardDescription>
+
                                 <CardAction onClick={handleAddtoCart}>
                                     <Button className='cursor-pointer'> <ShoppingCart /> Add To Cart</Button>
                                 </CardAction>
@@ -69,7 +101,6 @@ export default function BlogPostPage({
                         </Card>
                     ))
                 }
-
             </div>
         </div>
     )
